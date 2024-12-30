bab3183b5a3b161a01f4e73a99fd6ff78535324d
systic <llsysticll@gmail.com>
2024-12-30 20:39:19 +0900

    Movie Styles

---
+++ components/movie.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import styles from "../styles/movie.module.css";
+import Link from "next/link";
+
+interface IMovieProps {
+  id: string;
+  title: string;
+  poster_path: string;
+}
+
+export default function Movie({ id, title, poster_path }: IMovieProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(`/movies/${id}`);
+  };
+  return (
+    <div className={styles.movie}>
+      <img src={poster_path} alt={title} onClick={onClick} />
+      <Link href={`/movies/${id}`}>{title}</Link>
+    </div>
+  );
+}
