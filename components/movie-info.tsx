d94bc29218b5cbe53f2131c0a7990498febfda03
systic <llsysticll@gmail.com>
2024-12-31 19:08:22 +0900

    Add constants.ts

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,6 +1,5 @@
+import { API_URL } from "../app/constants";
 import styles from "../styles/movie-info.module.css";
-
-const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 export async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
